3c68a753e6991ef42e0b3694479aa7fa7abab916
Rubby Jane Doclara <rdoclara@gmail.com>
2022-10-07 10:15:21 +0800

    added first scenario to implement try catch

--- ContainNumberException.java
+++ ContainNumberException.java
@@ -1,3 +1,21 @@
-public class ContainNumberException {
+public class ContainNumberException extends Exception {
+
+    public ContainNumberException(String message) {
+        super(message);
+
+    }
     
+    public static boolean containsNum(String password) throws ContainNumberException{
+        boolean result = false;
+        for(int j=0; j < password.length(); j++){
+            if(Character.isDigit(password.charAt(j))){
+                result=true;
+            } 
+        }
+        if (result==false){
+            throw new ContainNumberException("\n Error! Password should contain a digit");  
+        }
+        return result;
+
+    }
 }
--- Main.java
+++ Main.java
@@ -9,18 +9,26 @@ public class Main {
         ArrayList<Customer> array = new ArrayList<Customer>(n); //arraylist is a generic class with a type parameter. Append class name in angle brackets  
 
         for(int i=0;i<n;i++){
-            System.out.println("Input name"+i+1+":");
-            String name = scanner.nextLine();
-            System.out.println("Input password"+i+1+":");
-            String password = scanner.nextLine();
-            Customer<String,String> cust = new Customer<String,String>(name,password);
-            System.out.println(cust.getName());
-            System.out.println(cust.getPassword());
-            array.add(cust);
+            System.out.println("Enter value for name " +  (i+1) +"");
+            String name = scanner.next();
+            System.out.println("Enter value for password " +  (i+1) +"");
+            String password = scanner.next();
+
+            try {
+               if( ContainNumberException.containsNum(password)==true){
+                Customer<String,String> cust = new Customer<String,String>(name,password);
+                array.add(cust);
+               }
+                
+            } catch (ContainNumberException e) {
+                System.out.println(e.getMessage());
+                
+
+                
+            }
         }
-
-       
-       
-
     }
+
+ 
+  
 }
