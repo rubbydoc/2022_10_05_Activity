c4fddbcde67774c3855da9d46f38588f867e6b64
Rubby Jane Doclara <rdoclara@gmail.com>
2022-10-07 09:16:22 +0800

    put objects inside an arraylist

--- Customer.java
+++ Customer.java
@@ -1,4 +1,26 @@
-public class Customer{
-    static String password;
-    static String name;
+public class Customer<T,T2>{
+    private T name;
+    private T2 password;
+
+    public T getName() {
+        return name;
+    }
+
+    public void setName(T name) {
+        this.name = name;
+    }
+
+    public T2 getPassword() {
+        return password;
+    }
+
+    public void setPassword(T2 password) {
+        this.password = password;
+    }
+
+    public Customer(T name, T2 password) {
+        this.name = name;
+        this.password = password;
+
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+    static Customer<String,String> customer = new Customer<String,String>("rubby","doc123");
     public static void main(String[] args){
-        
+        System.out.println("How many objects you want to create?");
+        int n = scanner.nextInt();
+        ArrayList<Customer> array = new ArrayList<Customer>(n); //arraylist is a generic class with a type parameter. Append class name in angle brackets  
+
+        for(int i=0;i<n;i++){
+            System.out.println("Input name"+i+1+":");
+            String name = scanner.nextLine();
+            System.out.println("Input password"+i+1+":");
+            String password = scanner.nextLine();
+            array.add(new Customer<String,String>(name,password));
+        }
+
+       
+
     }
 }
